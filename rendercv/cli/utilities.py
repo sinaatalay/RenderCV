e5c8a360a50b514715c996051154149674d4320c
Sina Atalay <dev@atalay.biz>
2025-01-01 03:16:42 +0300

    create a new function, parse_validation_errors

--- rendercv/cli/utilities.py
+++ rendercv/cli/utilities.py
@@ -17,6 +17,7 @@ from typing import Any, Optional
 import typer
 import watchdog.events
 import watchdog.observers
+import pydantic
 
 from .. import data, renderer
 from . import printer
@@ -173,6 +174,148 @@ def get_error_message_and_location_and_value_from_a_custom_error(
     if match:
         return match.group(1), match.group(2), match.group(3)
     return None, None, None
+
+
+def parse_validation_errors(
+    exception: pydantic.ValidationError,
+) -> list[dict[str, str]]:
+    """Take a Pydantic validation error, parse it, and return a list of error
+    dictionaries that contain the error messages, locations, and the input values.
+
+    Pydantic's `ValidationError` object is a complex object that contains a lot of
+    information about the error. This function takes a `ValidationError` object and
+    extracts the error messages, locations, and the input values.
+
+    Args:
+        exception: The Pydantic validation error object.
+
+    Returns:
+        A list of error dictionaries that contain the error messages, locations, and the
+        input values.
+    """
+    # This dictionary is used to convert the error messages that Pydantic returns to
+    # more user-friendly messages.
+    error_dictionary: dict[str, str] = {
+        "Input should be 'present'": (
+            "This is not a valid date! Please use either YYYY-MM-DD, YYYY-MM, or YYYY"
+            ' format or "present"!'
+        ),
+        "Input should be a valid integer, unable to parse string as an integer": (
+            "This is not a valid date! Please use either YYYY-MM-DD, YYYY-MM, or YYYY"
+            " format!"
+        ),
+        "String should match pattern '\\d{4}-\\d{2}(-\\d{2})?'": (
+            "This is not a valid date! Please use either YYYY-MM-DD, YYYY-MM, or YYYY"
+            " format!"
+        ),
+        "String should match pattern '\\b10\\..*'": (
+            'A DOI prefix should always start with "10.". For example,'
+            ' "10.1109/TASC.2023.3340648".'
+        ),
+        "URL scheme should be 'http' or 'https'": "This is not a valid URL!",
+        "Field required": "This field is required!",
+        "value is not a valid phone number": "This is not a valid phone number!",
+        "month must be in 1..12": "The month must be between 1 and 12!",
+        "day is out of range for month": "The day is out of range for the month!",
+        "Extra inputs are not permitted": (
+            "This field is unknown for this object! Please remove it."
+        ),
+        "Input should be a valid string": "This field should be a string!",
+        "Input should be a valid list": (
+            "This field should contain a list of items but it doesn't!"
+        ),
+    }
+
+    unwanted_texts = ["value is not a valid email address: ", "Value error, "]
+
+    # Check if this is a section error. If it is, we need to handle it differently.
+    # This is needed because how dm.validate_section_input function raises an exception.
+    # This is done to tell the user which which EntryType RenderCV excepts to see.
+    errors = exception.errors()
+    for error_object in errors.copy():
+        if (
+            "There are problems with the entries." in error_object["msg"]
+            and "ctx" in error_object
+        ):
+            location = error_object["loc"]
+            ctx_object = error_object["ctx"]
+            if "error" in ctx_object:
+                inner_error_object = ctx_object["error"]
+                if hasattr(inner_error_object, "__cause__"):
+                    cause_object = inner_error_object.__cause__
+                    cause_object_errors = cause_object.errors()
+                    for cause_error_object in cause_object_errors:
+                        # we use [1:] to avoid `entries` location. It is a location for
+                        # RenderCV's own data model, not the user's data model.
+                        cause_error_object["loc"] = tuple(
+                            list(location) + list(cause_error_object["loc"][1:])
+                        )
+                    errors.extend(cause_object_errors)
+
+    # some locations are not really the locations in the input file, but some
+    # information about the model coming from Pydantic. We need to remove them.
+    # (e.g. avoid stuff like .end_date.literal['present'])
+    unwanted_locations = ["tagged-union", "list", "literal", "int", "constrained-str"]
+    for error_object in errors:
+        location = [str(location_element) for location_element in error_object["loc"]]
+        new_location = [str(location_element) for location_element in location]
+        for location_element in location:
+            for unwanted_location in unwanted_locations:
+                if unwanted_location in location_element:
+                    new_location.remove(location_element)
+        error_object["loc"] = new_location  # type: ignore
+
+    # Parse all the errors and create a new list of errors.
+    new_errors: list[dict[str, str]] = []
+    for error_object in errors:
+        message = error_object["msg"]
+        location = ".".join(error_object["loc"])  # type: ignore
+        input = error_object["input"]
+
+        # Check if this is a custom error message:
+        custom_message, custom_location, custom_input_value = (
+            get_error_message_and_location_and_value_from_a_custom_error(message)
+        )
+        if custom_message is not None:
+            message = custom_message
+            if custom_location:
+                # If the custom location is not empty, then add it to the location.
+                location = f"{location}.{custom_location}"
+            input = custom_input_value
+
+        # Don't show unwanted texts in the error message:
+        for unwanted_text in unwanted_texts:
+            message = message.replace(unwanted_text, "")
+
+        # Convert the error message to a more user-friendly message if it's in the
+        # error_dictionary:
+        if message in error_dictionary:
+            message = error_dictionary[message]
+
+        # Special case for end_date because Pydantic returns multiple end_date errors
+        # since it has multiple valid formats:
+        if "end_date" in location:
+            message = (
+                "This is not a valid end date! Please use either YYYY-MM-DD, YYYY-MM,"
+                ' or YYYY format or "present"!'
+            )
+
+        # If the input is a dictionary or a list (the model itself fails to validate),
+        # then don't show the input. It looks confusing and it is not helpful.
+        if isinstance(input, dict | list):
+            input = ""
+
+        new_error = {
+            "loc": str(location),
+            "msg": message,
+            "input": str(input),
+        }
+
+        # if new_error is not in new_errors, then add it to new_errors
+        if new_error not in new_errors:
+            new_errors.append(new_error)
+
+    return new_errors
 
 
 def copy_templates(
