f8c3a8a27a04e13ea837453b93150d0b03264d46
Sina Atalay <dev@atalay.biz>
2025-01-04 16:42:30 +0300

    improve parse_validation_errors

--- rendercv/cli/utilities.py
+++ rendercv/cli/utilities.py
@@ -224,6 +224,10 @@ def parse_validation_errors(
         "Input should be a valid list": (
             "This field should contain a list of items but it doesn't!"
         ),
+        "value is not a valid color: string not recognised as a valid color": (
+            "This is not a valid color! Here are some examples of valid colors:"
+            ' "red", "#ff0000", "rgb(255, 0, 0)", "hsl(0, 100%, 50%)"'
+        ),
     }
 
     unwanted_texts = ["value is not a valid email address: ", "Value error, "]
