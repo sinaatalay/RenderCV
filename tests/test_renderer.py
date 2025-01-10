2bf2f965dcca8f07a5efe74b9ea49092f6450be0
Marcin <marcin.konow@lczyk.xyz>
2025-01-10 20:50:11 +0000

    update markdown_to_typst test

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -73,18 +73,57 @@ def test_escape_typst_characters(string, expected_string):
     ("markdown_string", "expected_typst_string"),
     [
         ("My Text", "My Text"),
-        ("**My** Text", "#[*My*] Text"),
-        ("*My* Text", "#[_My_] Text"),
-        ("***My*** Text", "#[*_My_*] Text"),
+        ("**My** Text", "#strong[My] Text"),
+        ("*My* Text", "#emph[My] Text"),
+        ("***My*** Text", "#strong[#emph[My]] Text"),
         ("[My](https://myurl.com) Text", '#link("https://myurl.com")[My] Text'),
         ("`My` Text", "`My` Text"),
         (
             "[**My** *Text* ***Is*** `Here`](https://myurl.com)",
-            '#link("https://myurl.com")[#[*My*] #[_Text_] #[*_Is_*] `Here`]',
+            (
+                '#link("https://myurl.com")[#strong[My] #emph[Text] #strong[#emph[Is]]'
+                " `Here`]"
+            ),
         ),
         (
             "Some other *** tests, which should be tricky* to parse!**",
-            "Some other #[*#[_ tests, which should be tricky_] to parse!*]",
+            "Some other #strong[#emph[ tests, which should be tricky] to parse!]",
+        ),
+        (
+            "One asterisk does not a quote* maketh",
+            "One asterisk does not a quote#sym.ast.basic maketh",
+        ),
+        (
+            "We can put asteri*sks in the middle of words",
+            (
+                "We can put asteri#sym.ast.basic#h(0pt, weak: true)sks in the middle of"
+                " words"
+            ),
+        ),
+        (
+            (
+                "If we want to escape \\*'s such that they don't become bold, we use a"
+                " backslash: \\*"
+            ),
+            (
+                "If we want to escape #sym.ast.basic#h(0pt, weak: true)'s such that"
+                " they don't become bold, we use a backslash: #sym.ast.basic#h(0pt,"
+                " weak: true)"
+            ),
+        ),
+        (
+            "Asterisk with a space after it does not need a zero-width space: * test",
+            (
+                "Asterisk with a space after it does not need a zero-width space:"
+                " #sym.ast.basic test"
+            ),
+        ),
+        (
+            "Asterisk with a space after it does not need a zero-width space: *test",
+            (
+                "Asterisk with a space after it does not need a zero-width space:"
+                " #sym.ast.basic#h(0pt, weak: true)test"
+            ),
         ),
     ],
 )
