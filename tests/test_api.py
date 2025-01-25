f6eb8b48c28e5d181dad56586fa100466efabe51
Sina Atalay <dev@atalay.biz>
2025-01-25 18:00:39 -0500

    tests: update api test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,6 @@
 import rendercv
 
 
-def test_rendercv():
-    assert isinstance(
-        rendercv.create_contents_of_a_typst_file({"cv": {"name": "John Doe"}}), str
-    )
+def test_create_contents_of_a_typst_file(input_file_path):
+    yaml_string = input_file_path.read_text()
+    assert isinstance(rendercv.create_contents_of_a_typst_file(yaml_string), str)
